6098f6a270674a04477d2e91ba1552526d083b56
Katrin Marschner <katrin.marschner@gmail.com>
2018-10-28 09:15:30 +0100

    load data with async/await

--- database.js
+++ database.js
@@ -1,11 +1,17 @@
 const fs = require('fs')
 
 module.exports = {
-  save: function(data){
-    fs.writeFileSync('data.json', JSON.stringify(data))
+  save(filename, data) {
+    fs.writeFileSync(filename, JSON.stringify(data))
   },
 
-  load() {
-    return JSON.parse(fs.readFileSync('data.json'))
+  load(filename) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filename, 'utf8', (err, contents) => {
+        if (err) return reject(err)
+
+        resolve(JSON.parse(contents))
+      })
+    })
   }
 }
--- index.js
+++ index.js
@@ -13,25 +13,35 @@ const BookOwner = require('./bookOwner')
 // erica.addBook("Emma", "Jane Austen")
 // michael.addBook("A Wild Sheep Chase", "Haruki Murakami")
 
-// Database.save([erica, michael])
+// Database.save('data.json', [erica, michael])
 
-const data = Database.load()
+const main = async () => {
+  const data = await Database.load('data.json')
 
-const erica = BookOwner.create(data.find( owner => owner.id === 'erica-smith' ))
-const michael = BookOwner.create(data.find( owner => owner.id === 'michael-becker' ))
+  const erica = BookOwner.create(data.find( owner => owner.id === 'erica-smith' ))
+  const michael = BookOwner.create(data.find( owner => owner.id === 'michael-becker' ))
 
-console.log(`${erica.name}'s books: ${erica.books.map(book => book.title).join(', ')}`)
-console.log(`${michael.name}'s books: ${michael.books.map(book => book.title).join(', ')}`)
+  console.log(`${erica.name}'s books: ${erica.books.map(book => book.title).join(', ')}`)
+  console.log(`${michael.name}'s books: ${michael.books.map(book => book.title).join(', ')}`)
 
-console.log(`${erica.name} wants to exchange a book.`)
-const book1 = erica.books[0]
-const book2 = michael.books[0]
-erica.requestExchange(michael, book2, book1)
-console.log(`${michael.name} has ${michael.requestedExchanges.length} request for book exchange.`)
+  console.log(`${erica.name} wants to exchange a book.`)
+  const book1 = erica.books[0]
+  const book2 = michael.books[0]
+  erica.requestExchange(michael, book2, book1)
+  console.log(`${michael.name} has ${michael.requestedExchanges.length} request for book exchange.`)
 
-console.log(`${michael.name} accepts the request.`)
-const exchange = michael.requestedExchanges[0]
-michael.acceptExchange(exchange)
+  console.log(`${michael.name} accepts the request.`)
+  const exchange = michael.requestedExchanges[0]
+  michael.acceptExchange(exchange)
 
-console.log(`${erica.name}'s books: ${erica.books.map(book => book.title).join(', ')}`)
-console.log(`${michael.name}'s books: ${michael.books.map(book => book.title).join(', ')}`)
+  console.log(`${erica.name}'s books: ${erica.books.map(book => book.title).join(', ')}`)
+  console.log(`${michael.name}'s books: ${michael.books.map(book => book.title).join(', ')}`)
+}
+
+(async () => {
+  try {
+    await main()
+  } catch (err) {
+    console.log(err)
+  }
+})()
