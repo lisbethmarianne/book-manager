a78afcbcacf362bc2e8158fa416b5d98c91f0924
Katrin Marschner <katrin.marschner@gmail.com>
2018-11-28 22:46:15 +0100

    dockerize all the things

--- mongo-connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/books', { useNewUrlParser: true })
-  .then(() => {
-    console.log('DB connected')
-  })
-  .catch((err) => {
-    console.error(err)
-  })
